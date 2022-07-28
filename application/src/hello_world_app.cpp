467c7642cdefb1c0ec9642011a984a163c57af72
ixi2101 <77992676+ixi2101@users.noreply.github.com>
2022-07-28 14:03:42 +0100

    Initial commit

---
+++ application/src/hello_world_app.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+auto dub(int x) -> int { return 2 * x; }
+
+
+
+int main() {
+  std::cout << "hellealal" << std::endl;
+  dub(3);
+  char a[10];
+  a[10] = 0;
+  
+  return 0;
+}
---
+++ application/test/hello_test.cpp
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+
+TEST(HelloTest, BasicAssertions){
+    EXPECT_EQ(2,2);
+    EXPECT_NE(3,2);
+    
+}
